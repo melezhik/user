0acf00b40e616b8775ca90d8d220e90f7207e74d
Spigell 7th <spigelly@gmail.com>
2017-03-11 12:26:50 +0300

    changed hook.bash - melezhik/user/#1

--- hook.bash
+++ hook.bash
@@ -1,13 +1,20 @@
-set -x 
 user=$(config name)
 action=$(config action)
 
-if [[ `grep  $user /etc/passwd` ]]; then
-  if [[ $action == delete ]]; then
-    run_story delete
-  else
-    run_story change 
-  fi
-else
-  run_story $action
+id $user &>/dev/null && is_user_exist=yes
+
+if [[ ! $is_user_exist == yes ]]; then
+  echo "creating new user - $user"
+  run_story create
 fi
+
+if [[ $is_user_exist == yes ]]; then
+  echo "modifyng existing user"
+  run_story change
+fi
+
+if [[ $is_user_exist == yes ]] && [[ $action == delete ]]; then
+  echo "delete user"
+  run_story delete
+fi
+
